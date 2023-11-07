374c9750bebc1181fdcc14e00dc074a80c6b2670
DogDoge <132619375+DogDoge0-0@users.noreply.github.com>
2023-11-07 10:54:15 -0500

    Update showPercentLink.js

--- JS/showPercent/showPercentLink.js
+++ JS/showPercent/showPercentLink.js
@@ -27,52 +27,49 @@ function insertPercent() {
 
 		// Function to apply grade calculation and event listeners
 		function applyGrade() {
-			chrome.storage.local.get('showPercent', function(result) {
-				if (result.showPercent === true) {
-					if (!percentText) {
-						grade = document.querySelector('.assignment-detail-status-label');
-						let gradeText = grade.textContent; // Extract the text content
+			if (!percentText) {
+				grade = document.querySelector('.assignment-detail-status-label');
+				console.log(grade)
+				let gradeText = grade.textContent; // Extract the text content
 
-						// Search for numbers using a specific pattern
-						let match = gradeText.match(/Graded: ([0-9.]+) of ([0-9.]+)/);
-						let grade1 = parseFloat(match[1]);
-						let grade2 = parseFloat(match[2]);
-						let gradeFinal = grade1 / grade2 * 100;
-						// Store the original and modified text content of the grade element
-						originalText = grade.textContent;
-						if (Math.abs(gradeFinal - Math.round(gradeFinal)) < 0.000001) {
-							percentText = ' Graded: ' + gradeFinal.toFixed(0) + '%';
-						}
-						else if (Math.round(gradeFinal) === gradeFinal) {
-							percentText = ' Graded: ' + gradeFinal.toFixed(0) + '%';
-						}
-						else {
-							percentText = ' Graded: ' + gradeFinal.toFixed(2) + '%';
-						}
-					}
-					// Display the grade
-					chrome.storage.local.get('showPercentSelect', function(result) {
-						var showPercentSelectValue = result.showPercentSelect;
+				// Search for numbers using a specific pattern
+				let match = gradeText.match(/Graded: ([0-9.]+) of ([0-9.]+)/);
+				let grade1 = parseFloat(match[1]);
+				let grade2 = parseFloat(match[2]);
+				let gradeFinal = grade1 / grade2 * 100;
+				// Store the original and modified text content of the grade element
+				originalText = grade.textContent;
+				if (Math.abs(gradeFinal - Math.round(gradeFinal)) < 0.000001) {
+					percentText = ' Graded: ' + gradeFinal.toFixed(0) + '%';
+				}
+				else if (Math.round(gradeFinal) === gradeFinal) {
+					percentText = ' Graded: ' + gradeFinal.toFixed(0) + '%';
+				}
+				else {
+					percentText = ' Graded: ' + gradeFinal.toFixed(2) + '%';
+				}
+			}
+			// Display the grade
+			chrome.storage.local.get('showPercentSelect', function(result) {
+				let showPercentSelectValue = result.showPercentSelect;
 
-						if (showPercentSelectValue == 1) {
-						} 
-						else if (showPercentSelectValue == 2) {
-							grade.addEventListener('mouseover', function() {
-								grade.textContent = percentText;
-							});
-
-							grade.addEventListener('mouseout', function() {
-								grade.textContent = originalText;
-							});
-						} 
-						else if (showPercentSelectValue == 3) {
-							grade.textContent = percentText;
-						} 
-						else {
-							alert('Grade Percent Converter unsuccessful, please send me the following error message:\nError: showPercentSelect unidentifiable, unable to parse input.');
-							return;
-						}
+				if (showPercentSelectValue == 1) {
+				}
+				else if (showPercentSelectValue == 2) {
+					grade.addEventListener('mouseover', function() {
+						grade.textContent = percentText;
 					});
+
+					grade.addEventListener('mouseout', function() {
+						grade.textContent = originalText;
+					});
+				}
+				else if (showPercentSelectValue == 3) {
+					grade.textContent = percentText;
+				}
+				else {
+					alert('Grade Percent Converter unsuccessful, please send me the following error message:\nError: showPercentSelect unidentifiable, unable to parse input.');
+					return;
 				}
 			});
 		}
