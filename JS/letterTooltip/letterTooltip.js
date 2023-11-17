cc471b7bf02830a5c59d2dc3456a80c8213fac36
DogDoge <132619375+DogDoge0-0@users.noreply.github.com>
2023-11-17 10:39:23 -0500

    Update letterTooltip.js

--- JS/letterTooltip/letterTooltip.js
+++ JS/letterTooltip/letterTooltip.js
@@ -1,22 +1,26 @@
 // Initialization
 let letterTooltipCheckbox = document.querySelector("#letterTooltip");
 chrome.storage.local.set({ "letterTooltipChecked": false });
+
 // Add an event listener to the checkbox to listen for changes in the checked state
 letterTooltipCheckbox.addEventListener("change", function() {
-  // Get the checked state
-  let letterTooltipChecked = letterTooltipCheckbox.checked;
-  // Store the checked state in chrome.storage
-  chrome.storage.local.set({ "letterTooltipChecked": letterTooltipChecked });
+    // Get the checked state
+    let letterTooltipChecked = letterTooltipCheckbox.checked;
+
+    // Update the tooltip preference in chrome.storage
+    chrome.storage.local.set({ "letterTooltipChecked": letterTooltipChecked });
 });
 
 // When the page loads, retrieve the checked state from chrome.storage and set the checked state of the checkbox
 window.addEventListener("load", function() {
-  chrome.storage.local.get("letterTooltipChecked", function(result) {
-    let letterTooltip = result.letterTooltipChecked;
-    if (letterTooltip) {
-      letterTooltipCheckbox.checked = true;
-    } else {
-      letterTooltipCheckbox.checked = false;
-    }
-  });
+    chrome.storage.local.get("letterTooltipChecked", function(result) {
+        let letterTooltipChecked = result.letterTooltipChecked;
+
+        // Set the checked state of the checkbox based on the stored preference
+        if (letterTooltipChecked) {
+            letterTooltipCheckbox.checked = true;
+        } else {
+            letterTooltipCheckbox.checked = false;
+        }
+    });
 });
